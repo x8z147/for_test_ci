fd32caa64e6d336244f937edcf2f99cc03df3367
x8z147 <a_sadovskiy@hbc.ru>
2014-05-18 17:11:55 +0400

    Update test_server001.js

--- test_server001.js
+++ test_server001.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World test 123\n tezis !!!');
+  res.end('Hello World test 123\n tezis 234523452345!!!');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
